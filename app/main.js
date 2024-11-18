c0d2519ad827c418b52e5b948d959dd1dfdd7e51
JackyJianger <158211567+JackyJianger@users.noreply.github.com>
2024-11-18 12:05:01 -0500

    Revert "a"
    This reverts commit ba2948f2416d68edce046b3672b85825fb3c8392.

--- app/main.js
+++ app/main.js
@@ -1,17 +1,1 @@
 import "./style.css";
-async function getAgent() {
-    try {
-        const response = await fetch('https://valorant-api.com/v1/agents');
-        if (response.status!=200) {
-            throw new Error(response);
-        } else{
-            const data = await response.json();
-            console.log(data);
-        }
-    } catch (error) {
-        console.log(error);
-        alert("Sorry could not find that agent")
-    }
-}
-
-getAgent()
