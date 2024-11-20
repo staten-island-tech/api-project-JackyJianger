124b75ab2bc7e7bdb4b2ce742a64e31456f4f958
JackyJianger <158211567+JackyJianger@users.noreply.github.com>
2024-11-20 12:30:24 -0500

    a

--- app/main.js
+++ app/main.js
@@ -1,17 +1,29 @@
 import "./style.css";
 
 const DOMSelectors = {
-    containerBox : document.querySelector(.main)
+    containerBox: document.querySelector('.main')
 }
 async function getAgent() {
     try {
         const response = await fetch('https://valorant-api.com/v1/agents');
-        if (response.status!=200) {
+        if (response.status != 200) {
             throw new Error(response);
         } else{
             const data = await response.json();
-            const agent = data.data.filter(agent => agent.isPlayableCharacter === true);
-            return agent
+            const agents = data.data.filter(agent => agent.isPlayableCharacter === true);
+            
+            agents.forEach(agent => {
+                console.log(agent);
+                DOMSelectors.containerBox.insertAdjacentHTML(
+                    "beforeend",
+                    `<div class="agentCards">
+                      <p>${agent.displayName}</p>
+                      <img class="images"src=${agent.bustProtrait}><img>
+                      <p>hi</p>
+                    </div>`
+                  );
+            });
+            
         }
     } catch (error) {
         console.log(error);
@@ -21,14 +33,3 @@ async function getAgent() {
 
 getAgent()
 
-function injectCardIntoDOM(getagent()) {    
-    DOMSelectors.cardContainer.insertAdjacentHTML(
-      "beforeend",
-      `<div class="agentCards">
-        <p>${agent.displayName}</p>
-        <img class="images"src=${agent.bustProtrait}><img>
-        <p>hi</p>
-      </div>`
-    );
-  }
-  injectCardIntoDOM(agent)
