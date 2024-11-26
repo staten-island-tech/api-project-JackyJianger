7b1d27e4ff99fb9d06ff2a1d48a5bdfdbce8ef01
JackyJianger <158211567+JackyJianger@users.noreply.github.com>
2024-11-26 12:32:17 -0500

    a

--- app/main.js
+++ app/main.js
@@ -16,7 +16,7 @@ async function getAgent() {
                 console.log(agent);
                 DOMSelectors.containerBox.insertAdjacentHTML(
                     "beforeend",
-                    `<div class="agentCards bg-slate-100 w-2/12 h-500 shadow-xl border-slate-50 p-5 m-1 flex flex-col justify-between items-center">
+                    `<div class="agentCards hover:scale-110 hover:shadow-2xl  bg-slate-100 w-2/12 h-500 shadow-xl border-slate-50 p-5 m-1 flex flex-col justify-between items-center">
                       <p>${agent.displayName}</p>
                       <img class="images"src=${agent.displayIcon}><img>
                       <p>${agent.role.displayName}</p>
@@ -32,4 +32,33 @@ async function getAgent() {
 }
 
 getAgent()
+
+
+async function getAgentInfo() {
+    try {
+        const response = await fetch('https://valorant-api.com/v1/agents');
+        if (response.status != 200) {
+            throw new Error(response);
+        } else{
+            const data = await response.json();
+            const agents = data.data.filter(agent => agent.isPlayableCharacter === true);
+            
+            agents.forEach(agent => {
+                console.log(agent);
+                DOMSelectors.containerBox.insertAdjacentHTML(
+                    "beforeend",
+                    `<div class="agentCards bg-slate-100 w-2/12 h-500 shadow-xl border-slate-50 p-5 m-1 flex flex-col justify-between items-center">
+                      <p>${agent.displayName}</p>
+                      <img class="images"src=${agent.displayIcon}><img>
+                      <p>${agent.role.displayName}</p>
+                    </div>`
+                  );
+            });
+            
+        }
+    } catch (error) {
+        console.log(error);
+        alert("Sorry could not find that agent")
+    }
+}
 
